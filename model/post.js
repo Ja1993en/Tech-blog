c0541bea0eab3c200f776c57064336834b3723c4
Basquiat <jlnmcneal@gmail.com>
2026-03-18 00:18:06 -0500

    Sequelize model for user p ost db table

---
+++ model/post.js
@@ -0,0 +1,46 @@
+const { Model, DataTypes, INTEGER } = require('sequelize');
+const sequelize = require('../config/connection.js');
+
+class Post extends Model { };
+
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            primaryKey: true,
+            allowNull: false,
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        date_published: {
+            type: DataTypes.DATE,
+            defaultValue: DataTypes.NOW,
+
+        },
+        author_id: {
+            type: DataTypes.INTEGER,
+            references: {
+            model: "user",
+            key: "id"
+            }
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'user',
+    }
+
+);
+
+
+module.exports = Post;
