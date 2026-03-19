b1edbfe236bc9c072878791004109c1e95cf1d0c
Basquiat <jlnmcneal@gmail.com>
2026-03-19 00:26:19 -0500

    Styling for login page and changes to handlebars login file

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -11,4 +11,14 @@ router.get('/', async (req, res) => {
     }
 })
 
+router.get('/login', async (req, res) => {
+    try {
+      res.render('login'); // Removed 'await'
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
+
+
 module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
 
-const homeRoutes = require('./homeRoutes.js');
+const homeRoutes = require('./homeRoutes');
 
 router.use('/', homeRoutes);
 
+router.get('/login', async (req, res) => {
+    try {
+      res.render('all'); // Removed 'await'
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 module.exports = router 
